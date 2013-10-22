8a9e101c62d9142b61fafc3d1e23e19aa07553f5
Saher Ahwal <saher@mit.edu>
2013-10-21 18:17:29 -0700

    mininova added spider by genspider

---
+++ torrent/torrent/spiders/mininova_spider.py
@@ -0,0 +1,21 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from torrent.items import TorrentItem
+
+class MininovaSpiderSpider(CrawlSpider):
+    name = 'mininova'
+    allowed_domains = ['mininova.org']
+    start_urls = ['http://www.mininova.org/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = TorrentItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
