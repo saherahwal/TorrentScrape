e5f3d23a6cda52499dae728a727bb8841885eca9
Tal Tchwella <tchwella@mit.edu>
2013-10-31 19:17:31 -0400

    cronjob for scriptdb

---
+++ torrent/cron/script-db.sh
@@ -0,0 +1,4 @@
+cd /home/neo/TorrentScrape/torrent
+#/usr/bin/scrapy crawl kat
+#/usr/bin/scrapy crawl piratebay
+python /home/neo/TorrentScrape/torrent/cron/db.py
--- torrent/cron/script.sh
+++ torrent/cron/script.sh
@@ -1,4 +1,4 @@
 cd /home/neo/TorrentScrape/torrent
 /usr/bin/scrapy crawl kat
 /usr/bin/scrapy crawl piratebay
-python /home/neo/TorrentScrape/torrent/cron/db.py
+#python /home/neo/TorrentScrape/torrent/cron/db.py
