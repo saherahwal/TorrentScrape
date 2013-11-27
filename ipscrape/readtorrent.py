7ff3e7e6eaa34e07565dbd4589edd535f20fb260
Tal Tchwella <tchwella@mit.edu>
2013-11-27 17:08:09 -0500

    timeout added

--- ipscrape/readtorrent.py
+++ ipscrape/readtorrent.py
@@ -10,7 +10,7 @@ def get_decoded_indexer_response(url, referer=None):
     """ return curl output download of torrent file """
     agent="'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4)'"
     proc = None
-    timeout = "120" #seconds timeout for whole operation
+    timeout = "120" #seconds timeout for whole curl operation
     if referer:
         proc = subprocess.Popen([
 "curl", "--globoff", "--compressed", "-A", agent, "-L", "--post302", "--referer", referer  , "--max-time", timeout, url], stdout= subprocess.PIPE)
